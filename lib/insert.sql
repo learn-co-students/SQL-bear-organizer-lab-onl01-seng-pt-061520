c72a0a05577c40ee209178fcdedac89b110d28ca
drewsview34 <drewsview34@outlook.com>
2020-08-12 22:17:37 -0700

    completed selects all female bears return name and age

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 18, "m", "dark", "chocolatey", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 21, "m", "red", "disorderly", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 25, "f", "yellow", "graceful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 35, "m", "brown", "orderly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 28, "f", "white", "lovely", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 40, "m", "green", "creepy", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 30, "f", "black", "friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, NULL, NULL, NULL, NULL, NULL);

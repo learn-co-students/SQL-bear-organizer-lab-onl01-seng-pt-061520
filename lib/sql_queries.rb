8406e6e102527e11a10d8105f22c8aa5449c5ea4
drewsview34 <drewsview34@outlook.com>
2020-08-13 06:59:37 -0700

    finished lab

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
   "SELECT name, age 
    FROM bears
-   WHERE gender IS 'F';"
+   WHERE gender IS 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -32,17 +32,22 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color
+  "SELECT color,
    COUNT(color)
    FROM bears
    GROUP BY color
+   ORDER BY color
    DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament)
+   FROM bears
+   WHERE temperament IS 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT *
+   FROM bears
+   WHERE name IS NULL"
 end

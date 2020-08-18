c066476c99b6dc697e80061bd40710e287859665
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-08-17 22:30:31 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES
+("Mr CHocolate", 2, "Male", "Brown", "Nice", 1),
+("Rowdy", 2, "Male", "Brown", "Nice", 1),
+("Tabitha", 2, "Male", "Brown", "Nice", 1),
+("Sergeant Brown", 2, "Male", "Dark Brown", "Nice", 1),
+("Melissa", 2, "Male", "Brown", "Nice", 1),
+("Grinch", 2, "Male", "Green", "Nice", 0),
+("Wendy", 2, "Male", "Brown", "Nice", 1),
+(NULL, 2, "Male", "Brown", "Nice", 1);
